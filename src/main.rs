448423deb41c941d114932b8bdfef21dfa229b9a
apple115 <2567031030@qq.com>
2023-11-09 11:48:47 +0800

    updata select

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlparser::ast::{ColumnDef, DataType, Statement, Value};
+use sqlparser::ast::{ColumnDef, DataType, Statement};
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 use std::collections::HashMap;
@@ -22,11 +22,13 @@ fn main() {
         name VARCHAR(255),
         age INT
     );
-    INSERT INTO my_table VALUES (1, 'Alice', 30);";
+    INSERT INTO my_table VALUES (1, 'Alice', 30);
+    SELECT id, name FROM my_table;
+";
 
     let dialect = GenericDialect {};
     let ast = Parser::parse_sql(&dialect, sql_query).expect("Failed to parse SQL");
-    //println!("{:#?}", ast);
+    // println!("{:#?}", ast);
 
     let mut database = Database {
         file_path: String::from("/your/database/file/path"),
@@ -84,6 +86,55 @@ fn main() {
                                     .collect();
                             table.data.push(row_values);
                         }
+                    }
+                }
+            }
+
+            Statement::Query(ref query) => {
+                if let sqlparser::ast::Query { body, .. } = query.as_ref() {
+                    if let sqlparser::ast::SetExpr::Select(select) = &**body {
+                        if let sqlparser::ast::TableFactor::Table { name, .. } =
+                            &select.from[0].relation
+                        {
+                            let table_name = name.to_string();
+                            let selected_columns: Vec<String> = select
+                                .projection
+                                .iter()
+                                .map(|p| match p {
+                                    sqlparser::ast::SelectItem::UnnamedExpr(expr) => match expr {
+                                        sqlparser::ast::Expr::Identifier(ident) => {
+                                            ident.value.clone()
+                                        }
+                                        _ => panic!("Unsupported selection expression"),
+                                    },
+                                    _ => panic!("Unsupported selection item"),
+                                })
+                                .collect();
+
+                            println!("Table name: {}", table_name);
+                            println!("Selected columns: {:?}", selected_columns);
+                            // Output the selected columns' data only
+                            let table_data = &database.tables[&table_name].data;
+                            let selected_table_data: Vec<Vec<String>> = table_data
+                                .iter()
+                                .map(|row| {
+                                    let mut selected_row_data: Vec<String> = Vec::new();
+                                    for (col, val) in row.iter().enumerate() {
+                                        if selected_columns
+                                            .contains(&database.tables[&table_name].columns[col])
+                                        {
+                                            selected_row_data.push(val.clone());
+                                        }
+                                    }
+                                    selected_row_data
+                                })
+                                .collect();
+                            println!("Selected table data: {:?}", selected_table_data);
+                        } else {
+                            panic!("No table name provided");
+                        }
+                    } else {
+                        panic!("Unsupported query type");
                     }
                 }
             }
