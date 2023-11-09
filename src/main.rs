6f1a2f76a11d69cd993d9621728636dfb911aa26
apple115 <2567031030@qq.com>
2023-11-09 10:13:13 +0800

    update Insert

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlparser::ast::{ColumnDef, DataType, Statement};
+use sqlparser::ast::{ColumnDef, DataType, Statement, Value};
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 use std::collections::HashMap;
@@ -21,35 +21,74 @@ fn main() {
         id INT,
         name VARCHAR(255),
         age INT
-    );";
+    );
+    INSERT INTO my_table VALUES (1, 'Alice', 30);";
 
     let dialect = GenericDialect {};
     let ast = Parser::parse_sql(&dialect, sql_query).expect("Failed to parse SQL");
+    //println!("{:#?}", ast);
 
-    if let Statement::CreateTable { name, columns, .. } = &ast[0] {
-        let table_name = name.to_string();
-        let columns_info: Vec<(String, DataType)> = columns
-            .iter()
-            .map(|col| {
-                let ColumnDef {
-                    name, data_type, ..
-                } = col;
-                (name.to_string(), data_type.clone())
-            })
-            .collect();
-        let column_names: Vec<String> = columns_info.iter().map(|(name, _)| name.clone()).collect();
-        let new_table = Table {
-            name: table_name.clone(),
-            columns: column_names,
-            data: vec![],
-        };
-        let mut database = Database {
-            file_path: String::from("/your/database/file/path"),
-            tables: HashMap::new(),
-        };
-        database.tables.insert(table_name, new_table);
+    let mut database = Database {
+        file_path: String::from("/your/database/file/path"),
+        tables: HashMap::new(),
+    };
 
-        // Display the database and table info
-        println!("Database: {:#?}", database);
+    for statement in ast {
+        match statement {
+            Statement::CreateTable { name, columns, .. } => {
+                let table_name = name.to_string();
+                let columns_info: Vec<(String, DataType)> = columns
+                    .iter()
+                    .map(|col| {
+                        let ColumnDef {
+                            name, data_type, ..
+                        } = col;
+                        (name.to_string(), data_type.clone())
+                    })
+                    .collect();
+                let column_names: Vec<String> =
+                    columns_info.iter().map(|(name, _)| name.clone()).collect();
+                let new_table = Table {
+                    name: table_name.clone(),
+                    columns: column_names,
+                    data: vec![],
+                };
+                database.tables.insert(table_name, new_table);
+
+                // Display the database and table info
+            }
+
+            Statement::Insert {
+                table_name, source, ..
+            } => {
+                let table_name = table_name.to_string();
+                let table = database
+                    .tables
+                    .get_mut(&table_name)
+                    .expect("Table does not exist");
+
+                if let sqlparser::ast::Query { body, .. } = *source {
+                    if let sqlparser::ast::SetExpr::Values(values) = *body {
+                        for row in values.rows {
+                            let row_values: Vec<String> =
+                                row.into_iter()
+                                    .map(|val| match val {
+                                        sqlparser::ast::Expr::Value(
+                                            sqlparser::ast::Value::Number(n, _),
+                                        ) => n.to_string(),
+                                        sqlparser::ast::Expr::Value(
+                                            sqlparser::ast::Value::SingleQuotedString(s),
+                                        ) => s,
+                                        _ => panic!("Unsupported value type"),
+                                    })
+                                    .collect();
+                            table.data.push(row_values);
+                        }
+                    }
+                }
+            }
+            _ => panic!("Unsupported SQL statement"),
+        }
     }
+    println!("{:#?}", database);
 }
