3ebeff0eb59341bd774a0e6c3ff7ebfa7a7f4d0e
apple115 <2567031030@qq.com>
2023-11-10 14:59:37 +0800

    update cmdline

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,17 @@ fn main() {
         name VARCHAR(255),
         age INT
     );
+DROP TABLE table_name
 
 
-    INSERT INTO my_table VALUES (1, 'Alice', 30);
-    INSERT INTO my_table VALUES (3, 'Charlie', 25);
     
 ";
+
+    let ast = parse_sql_queries(&sql_query); // Parse the SQL queries
+    println!("{:#?}", ast);
+
+    // INSERT INTO my_table VALUES (1, 'Alice', 30);
+    // INSERT INTO my_table VALUES (3, 'Charlie', 25);
 
     // INSERT INTO my_table VALUES (2, 'Bob', 20);
     // INSERT INTO my_table VALUES (3, 'Charlie', 25);
@@ -39,18 +44,14 @@ fn main() {
     //
     //SELECT * FROM my_other_table;
     let mut database = Database::new(); // Create a new database
-                                        // let ast = parse_sql_queries(sql_query); // Parse the SQL queries
-                                        //                                         // println!("{:#?}", ast); // Print the parsed queries
-                                        // execute_queries(&mut database, ast); // Execute the parsed queries on the database
     database.load();
-    println!("{:#?}", database); // Print the database}
-                                 //let _ = database.save();
-                                 // loop {
-                                 //     print!("dbms> ");
-                                 //     io::stdout().flush().unwrap(); // Flush the output
-                                 //
-                                 //     let mut query = String::new();
-                                 //     io::stdin().read_line(&mut query).unwrap();
-                                 //
-                                 // }
+    // println!("{:#?}", database); // Print the database}
+    loop {
+        print!("dbms> ");
+        io::stdout().flush().unwrap(); // Flush the output
+        let mut query = String::new();
+        io::stdin().read_line(&mut query).unwrap();
+        let ast = parse_sql_queries(&query); // Parse the SQL queries
+        execute_queries(&mut database, ast); // Execute the parsed queries on the database
+    }
 }
